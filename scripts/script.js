5988bdc863d22dbaf17401300d07a6f213dd7e5a
Artem Nikiforov <avnikiforov@inbox.ru>
2023-01-11 19:26:52 +0400

    feat: сверстать проектную работу #4

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,54 @@
-let profile = document.querySelector('.profile');
-let profileEditButton = profile.querySelector('.profile__info-edit-button');
+const popupElement = document.querySelector('.popup');
+const popupCloseButton = popupElement.querySelector('.popup__close');
+const popupOpenButton = document.querySelector('.profile__info-edit-button');
+const formElement = document.querySelector('.popup__container');
+const nameInput = formElement.querySelector('.popup__name');
+const jobInput = formElement.querySelector('.popup__about');
+const profileName = document.querySelector('.profile__info-title');
+const profileSubtitle = document.querySelector('.profile__info-subtitle');
+const popupSave = document.querySelector('.popup__button');
+
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened")
+ }
+
+function closePopup(popup) {
+   popup.classList.remove("popup_opened")
+ }
+
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+
+  closePopup(popupElement);
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+popupOpenButton.addEventListener("click", () => {
+
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  openPopup(popupElement)
+})
+
+popupCloseButton.addEventListener("click", () => {
+
+  closePopup(popupElement)
+})
+
+popupSave.addEventListener("click", () => {
+
+  closePopup(popupElement)
+})
+
+
+
+
+
+
 
